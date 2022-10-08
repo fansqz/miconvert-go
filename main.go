aaf51abc3740d77c2685b0b286ec8e1b9a325fbf
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-10-08 16:03:45 +0800

    feat:添加main启动文件

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
-package miconvert_go_backend
+package main
+
+import (
+	"fmt"
+	"miconvert-go/db"
+	"miconvert-go/routers"
+	"miconvert-go/setting"
+	"os"
+)
+
+func main() {
+	//获取参数
+	if len(os.Args) < 2 {
+		fmt.Println("参数错误")
+		return
+	}
+	path := os.Args[1]
+	//加载配置
+	if err := setting.Init(path); err != nil {
+		fmt.Println("加载配置文件出错")
+		return
+	}
+	//连接数据库
+	if err := db.InitMysql(setting.Conf.MySqlConfig); err != nil {
+		fmt.Println("数据库连接失败")
+	}
+	//程序退出时关闭mysql
+	defer db.CloseMysql()
+	//模型绑定
+	//todo:暂时不需要mysql？
+	db.DB.AutoMigrate()
+	//注册路由
+	routers.Run()
+}
