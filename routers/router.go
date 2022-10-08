19ef02d6b0d2adf993664f232e9afd63f08e42a8
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-10-08 15:48:11 +0800

    feat:添加路由相关

---
+++ routers/router.go
@@ -0,0 +1,34 @@
+// Package routers
+// @Author: fzw
+// @Create: 2022/10/8
+// @Description: 路由相关
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"miconvert-go/controllers"
+	"miconvert-go/setting"
+)
+
+//
+// Run
+//  @Description: 启动路由
+//
+func Run() {
+	if setting.Conf.Release {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.Default()
+	//设置静态文件位置
+	r.Static("/static", "/")
+
+	//文件转换相关
+	convert := r.Group("/convert")
+	{
+		convertController := controllers.NewConvertController()
+		convert.POST("/convertFiles", convertController.ConvertFiles)
+		convert.GET("/getSupportFormat", convertController.GetSupportFormat)
+		convert.GET("/downloadFiles", convertController.DownloadFiles)
+	}
+}
