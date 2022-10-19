d2a71b055bd1e8c4a29ee099920d9dbe464e2041
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-10-19 15:27:42 +0800

    feat:添加用户对象和dao

---
+++ dao/user_dao.go
@@ -0,0 +1,46 @@
+package dao
+
+import (
+	"miconvert-go/db"
+	"miconvert-go/models"
+)
+
+//GetUserByName 根据用户名读取一条数据
+func GetUserByName(username string) (*models.User, error) {
+	//写sql语句
+	sqlStr := "select id,username,password,email from users where username = ?"
+	//执行
+	row := db.DB.Raw(sqlStr, username)
+	user := &models.User{}
+	row.Scan(&user)
+	return user, nil
+}
+
+// CheckUserNameInDb 检测用户名是否存在
+func CheckUserNameInDb(username string) bool {
+	sqlStr := "select name from users where username = ?"
+	//执行
+	row := db.DB.Raw(sqlStr, username)
+	var username2 string
+	row.Scan(&username2)
+	return username2 != ""
+}
+
+// CheckEmailInDb 检测邮箱是否存在
+func CheckEmailInDb(email string) bool {
+	sqlStr := "select email from users where email = ?"
+	row := db.DB.Raw(sqlStr, email)
+	var email2 string
+	row.Scan(&email2)
+	return email2 != ""
+
+}
+
+//SaveUser 向数据库中插入用户信息
+func SaveUser(username string, password string, email string) error {
+	//写sql语句
+	sqlStr := "insert into users(username,password,email) values(?,?,?)"
+	//执行
+	db.DB.Exec(sqlStr, username, password, email)
+	return nil
+}
--- models/format_convert.go
+++ models/format_convert.go
@@ -9,8 +9,8 @@ package models
 // @Description: 记录格式，和格式转换
 //
 type FormatConvert struct {
-	ID          int    `gorm:"primary_key;AUTO_INCREMENT;column:id"`        //格式id
-	InFormat    string `gorm:"colum:in_format;unique_index:format_convert"` //格式类型
-	OutFormat   string `gorm:"out_format;unique_index:format_convert"`      //可转换的格式类型
-	ConvertUtil int    `gorm:"convert_util;unique_index:format_convert"`    //使用的工具
+	ID          int    `gorm:"primary_key;AUTO_INCREMENT;column:id"`           //格式id
+	InFormat    string `gorm:"colum:in_format;unique_index:format_convert"`    //格式类型
+	OutFormat   string `gorm:"colum:out_format;unique_index:format_convert"`   //可转换的格式类型
+	ConvertUtil int    `gorm:"colum:convert_util;unique_index:format_convert"` //使用的工具
 }
