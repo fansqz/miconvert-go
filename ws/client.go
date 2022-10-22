5318197edeb3df6c1e0e9b2b9906bf0693f8250d
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-10-22 22:19:30 +0800

    fix:修改ws，维护简单的map

--- ws/client.go
+++ ws/client.go
@@ -2,8 +2,6 @@ package ws
 
 import "C"
 import (
-	"log"
-	"miconvert-go/controllers"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -25,101 +23,7 @@ const (
 	maxMessageSize = 512
 )
 
-var (
-	newline = []byte{'\n'}
-	space   = []byte{' '}
-)
-
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-}
-
-// Client is a middleman between the websocket connection and the hub.
-type Client struct {
-	// The websocket connection.
-	conn *websocket.Conn
-
-	//sessionID用于标识
-	sessionID string
-
-	//待发送数据
-	send chan []byte
-}
-
-//
-// readPump
-//  @Description: 循环读取信息，，监听ws是否断开
-//  @receiver c
-//
-func (c *Client) readPump() {
-	defer func() {
-		controllers.ConvertManager.ReleaseSource(c.sessionID)
-		err := c.conn.Close()
-		if err != nil {
-			return
-		}
-	}()
-	c.conn.SetReadLimit(maxMessageSize)
-	c.conn.SetReadDeadline(time.Now().Add(pongWait))
-	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
-	for {
-		_, _, err := c.conn.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
-			}
-			break
-		}
-		//处理数据
-
-	}
-}
-
-// writePump pumps messages from the hub to the websocket connection.
-//
-// A goroutine running writePump is started for each connection. The
-// application ensures that there is at most one writer to a connection by
-// executing all writes from this goroutine.
-func (c *Client) writePump() {
-	ticker := time.NewTicker(pingPeriod)
-	defer func() {
-		ticker.Stop()
-		//ws断开，告知convert_manager删除该client的所有数据
-		controllers.ConvertManager.ReleaseSource(c.sessionID)
-		c.conn.Close()
-	}()
-	for {
-		select {
-		case message, ok := <-c.send:
-			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if !ok {
-				// The hub closed the channel.
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
-
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				return
-			}
-			w.Write(message)
-
-			// Add queued chat messages to the current websocket message.
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-c.send)
-			}
-
-			if err := w.Close(); err != nil {
-				return
-			}
-		case <-ticker.C:
-			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				return
-			}
-		}
-	}
 }
--- ws/ws.go
+++ ws/ws.go
@@ -1,9 +1,15 @@
 package ws
 
 import (
+	"github.com/gorilla/websocket"
 	"log"
+	"miconvert-go/utils"
 	"net/http"
+	"strings"
+	"time"
 )
+
+var wsMap = make(map[int]*websocket.Conn)
 
 //
 // ServeWs
@@ -18,10 +24,29 @@ func ServeWs(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
-	client := &Client{conn: conn, send: make(chan []byte, 256)}
+	//获取token
+	array := strings.Split(r.URL.String(), ",")
+	token := array[len(array)-1]
+	user, _ := utils.ParseToken(token)
+	wsMap[user.ID] = conn
 
-	// Allow collection of memory referenced by the caller by doing all work in
-	// new goroutines.
-	go client.writePump()
-	go client.readPump()
+	go listenClose(user.ID, conn)
+}
+
+func listenClose(userid int, ws *websocket.Conn) {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		ws.Close()
+		delete(wsMap, userid)
+	}()
+	for {
+		select {
+		case <-ticker.C:
+			ws.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := ws.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
 }
