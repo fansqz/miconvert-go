e7bf1eb2eb4a40a1c590937d1543439ac99aa35b
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-10-23 10:14:58 +0800

    feat:用户解析文件

--- controllers/user_convert_handler.go
+++ controllers/user_convert_handler.go
@@ -1,19 +1,26 @@
 package controllers
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"io"
+	"log"
 	"miconvert-go/dao"
+	"miconvert-go/models"
 	r "miconvert-go/models/result"
+	"miconvert-go/setting"
 	"miconvert-go/utils"
+	"miconvert-go/ws"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type UserConvertController interface {
 	//获取用户文件列表
 	ListFile(ctx *gin.Context)
 	//用户批量删除文件
-	DeleteFile(ctx *gin.Context)
+	DeleteFiles(ctx *gin.Context)
 	//ConvertFile 添加文件进行解析,同步解析
 	ConvertFile(ctx *gin.Context)
 	// DownloadFile 下载解析文件，同步解析时使用
@@ -40,7 +47,7 @@ func (c *userConvertController) ListFile(ctx *gin.Context) {
 	result.SuccessData(userFiles)
 }
 
-func (c *userConvertController) DeleteFile(ctx *gin.Context) {
+func (c *userConvertController) DeleteFiles(ctx *gin.Context) {
 	result := r.NewResult(ctx)
 	stringIDs := ctx.QueryArray("ids")
 	//遍历每一个ids删除文件
@@ -61,4 +68,52 @@ func (c *userConvertController) DeleteFile(ctx *gin.Context) {
 		dao.DeleteUserFile(userFile.ID)
 	}
 	result.SuccessMessage("删除成功!")
+}
+
+func (c *userConvertController) ConvertFile(ctx *gin.Context) {
+	result := r.NewResult(ctx)
+	token := ctx.GetHeader("token")
+	user, _ := utils.ParseToken(token)
+	outFormat := ctx.PostForm("outFormat")
+	userFile := &models.UserFile{UserID: user.ID}
+	//保存文件到temp
+	file, head, err := ctx.Request.FormFile("file")
+	defer file.Close()
+	infilename := utils.GetUUID() + "_" + head.Filename
+	infilePath := setting.Conf.UserInPath + "/" + infilename
+	os.Mkdir(setting.Conf.UserInPath, os.ModePerm)
+	f, err := os.Create(infilePath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+	//添加到dao
+	userFile.InFileName = head.Filename
+	userFile.InFilePath = infilePath
+	userFile.OutFileName = strings.Split(head.Filename, ".")[0] + "." + outFormat
+	userFile.State = models.CONVERTING
+	dao.InsertUserFile(userFile)
+	//进行异步解析
+	go func() {
+		outFilePath, convertErr := utils.Convert(infilePath, setting.Conf.UserOutPath, outFormat)
+		if convertErr != nil {
+			userFile.OutFilePath = outFilePath
+			userFile.State = models.FALSE
+		} else {
+			userFile.OutFileName = outFilePath
+			userFile.State = models.SUCCESS
+		}
+		//通过ws发送信息给用户
+		dao.UpdateUserFile(userFile)
+		//发送数据给前端
+		userFiles := dao.ListUserFileByUserId(user.ID)
+		json, _ := json.Marshal(r.ResultCont{
+			Code: 200,
+			Data: userFiles,
+		})
+		ws.WSManager.SendMessage(user.ID, json)
+	}()
+	result.SuccessData("文件已添加")
 }
--- ws/conn_manager.go
+++ ws/conn_manager.go
@@ -5,30 +5,36 @@ import (
 	"sync"
 )
 
+// todo:直接加锁效率低
 type wsManager struct {
 	wsMap map[int]*websocket.Conn
 	mutex sync.Mutex
 }
 
-func (wm *wsManager) sendMessage(userId int, bytes []byte) {
+func (wm *wsManager) SendMessage(userId int, bytes []byte) {
 	wm.mutex.Lock()
 	conn := wm.wsMap[userId]
+	wm.mutex.Unlock()
+	if conn == nil {
+		return
+	}
 	w, err := conn.NextWriter(websocket.TextMessage)
 	if err != nil {
-		wm.deleteConn(userId)
+		wm.DeleteConn(userId)
+		return
 	}
 	w.Write(bytes)
 }
 
-func (wm *wsManager) deleteConn(userId int) {
+func (wm *wsManager) DeleteConn(userId int) {
+	WSManager.mutex.Lock()
 	ws := wm.wsMap[userId]
 	ws.Close()
-	WSManager.mutex.Lock()
 	delete(WSManager.wsMap, userId)
 	WSManager.mutex.Unlock()
 }
 
-func (wm *wsManager) addConn(userId int, ws *websocket.Conn) {
+func (wm *wsManager) AddConn(userId int, ws *websocket.Conn) {
 	wm.mutex.Lock()
 	wm.wsMap[userId] = ws
 	wm.mutex.Unlock()
