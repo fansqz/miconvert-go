5c27ff05a883e0aa694d066f331bcc960ce94400
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-01-31 10:43:17 +0800

    更新

--- go.mod
+++ go.mod
@@ -4,15 +4,18 @@ go 1.18
 
 require (
 	github.com/chilts/sid v0.0.0-20190607042430-660e94789ec9
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gen2brain/go-fitz v1.20.2
 	github.com/gin-gonic/gin v1.8.1
 	github.com/gomodule/redigo v1.8.9
 	github.com/gorilla/websocket v1.5.0
 	github.com/jinzhu/gorm v1.9.16
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/ini.v1 v1.67.0
 )
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -27,12 +30,10 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- utils/convert_util.go
+++ utils/convert_util.go
@@ -6,10 +6,14 @@ package utils
 
 import (
 	"fmt"
+	"github.com/gen2brain/go-fitz"
+	"image/jpeg"
 	"log"
 	"miconvert-go/dao"
+	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"strings"
 )
 
@@ -66,6 +70,85 @@ func Pdf2docxConvert(fileSrcPath string, fileOutDir string) (fileOutPath string,
 	//返回数据
 	log.Println("文件转换成功", string(byteByState))
 	return fileOutPath, nil
+}
+
+//
+// GoFitzConvert
+//  @Description: 利用go-fitz进行转换
+// 支持pdf -> txt，pdf -> jpg, pdf -> html
+//  @param fileSrcPath
+//  @param fileOutDir
+//  @param toFormat
+//  @return fileOutPath
+//  @return err
+//
+func GoFitzConvert(fileSrcPath string, fileOutDir string, toFormat string) (fileOutPath string, e error) {
+	doc, err := fitz.New(fileSrcPath)
+	if err != nil {
+		panic(err)
+	}
+	defer doc.Close()
+	//获取输出文件名称
+	// Extract pages as images
+	for n := 0; n < doc.NumPage(); n++ {
+		img, err := doc.Image(n)
+		if err != nil {
+			panic(err)
+		}
+
+		f, err := os.Create(filepath.Join(fileOutDir, fmt.Sprintf("test%03d.jpg", n)))
+		if err != nil {
+			panic(err)
+		}
+
+		err = jpeg.Encode(f, img, &jpeg.Options{jpeg.DefaultQuality})
+		if err != nil {
+			panic(err)
+		}
+
+		f.Close()
+	}
+
+	// Extract pages as text
+	for n := 0; n < doc.NumPage(); n++ {
+		text, err := doc.Text(n)
+		if err != nil {
+			panic(err)
+		}
+
+		f, err := os.Create(filepath.Join(fileOutDir, fmt.Sprintf("test%03d.txt", n)))
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = f.WriteString(text)
+		if err != nil {
+			panic(err)
+		}
+
+		f.Close()
+	}
+
+	// Extract pages as html
+	for n := 0; n < doc.NumPage(); n++ {
+		html, err := doc.HTML(n, true)
+		if err != nil {
+			panic(err)
+		}
+
+		f, err := os.Create(filepath.Join(fileOutDir, fmt.Sprintf("test%03d.html", n)))
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = f.WriteString(html)
+		if err != nil {
+			panic(err)
+		}
+
+		f.Close()
+	}
+
 }
 
 //
